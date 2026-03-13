fb250ec152b8e97d64ceb88863b403e72848f98e
Siam <hellosifat.mail@gmail.com>
2026-03-13 11:27:18 +0600

    fix db connection

--- db.js
+++ db.js
@@ -9,7 +9,11 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0,
   charset: 'utf8mb4',
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+  connectTimeout: 30000,
 })
 
 module.exports = pool
